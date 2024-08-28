ba1985ff8cd6eb11e019f7b5d3cb797e730504bb
root <rastorgu98@gmail.com>
2024-08-28 13:46:54 +0300

    DZ_16_1 init_test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,22 @@
 import pytest
 
-from src.main import Category, Product
+from src.main import Smartphone, Product, LawnGrass
 
 
 @pytest.fixture
 def samsung():
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture
+def smartphone():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                      "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def grass():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
 
 
 # @pytest.fixture
@@ -16,13 +27,11 @@ def samsung():
 #                     1)
 
 
-
 @pytest.fixture
 def new_product():
     return (
         "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.Iphone 15, 210000.0 руб. Остаток: 8 шт.Xiaomi Redmi "
         "Note 11, 31000.0 руб. Остаток: 14 шт.")
-
 
 # @pytest.fixture
 # def product1():
